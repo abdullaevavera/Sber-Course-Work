f0bc3076fa9b904a7ebc49537375bd7ad893a557
Вера Абдуллаева <sinang0601@gmail.com>
2023-11-15 01:31:34 +0700

    Работа над DocumentServiceImpl: методы delete, deleteAll и get. Добавление файлов конфигурации.

--- backend/src/main/java/ru/abdullaeva/sber/backend/documents/service/DocumentServiceImpl.java
+++ backend/src/main/java/ru/abdullaeva/sber/backend/documents/service/DocumentServiceImpl.java
@@ -2,10 +2,10 @@ package ru.abdullaeva.sber.backend.documents.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import ru.abdullaeva.sber.backend.documents.dto.DocumentDto;
 import ru.abdullaeva.sber.backend.documents.dto.IdDto;
 import ru.abdullaeva.sber.backend.documents.mapper.CustomDocumentMapper;
-import ru.abdullaeva.sber.backend.documents.mapper.DocumentMapper;
 import ru.abdullaeva.sber.backend.documents.model.Document;
 import ru.abdullaeva.sber.backend.documents.model.StatusEnum;
 import ru.abdullaeva.sber.backend.documents.repository.DocumentRepository;
@@ -16,16 +16,15 @@ import java.util.List;
 import java.util.Set;
 
 @Slf4j
+@Service
 public class DocumentServiceImpl implements DocumentService {
 
     private final DocumentRepository documentRepository;
-    private final DocumentMapper documentMapper;
     private final CustomDocumentMapper customDocumentMapper;
 
     @Autowired
-    public DocumentServiceImpl(DocumentRepository documentRepository, DocumentMapper documentMapper, CustomDocumentMapper customDocumentMapper) {
+    public DocumentServiceImpl(DocumentRepository documentRepository, CustomDocumentMapper customDocumentMapper) {
         this.documentRepository = documentRepository;
-        this.documentMapper = documentMapper;
         this.customDocumentMapper = customDocumentMapper;
     }
 
@@ -48,12 +47,12 @@ public class DocumentServiceImpl implements DocumentService {
 
     @Override
     public void deleteAll(Set<Long> ids) {
-
+        ids.forEach(id -> documentRepository.deleteById(id.intValue()));
     }
 
     @Override
     public void delete(Long id) {
-
+        documentRepository.deleteById(id.intValue());
     }
 
     @Override
@@ -66,11 +65,11 @@ public class DocumentServiceImpl implements DocumentService {
         List<Document> documentList = documentRepository.findAll();
         List<DocumentDto> documentDtoList = new ArrayList<>();
 
-        if(documentList.isEmpty()) {
+        if (documentList.isEmpty()) {
             log.warn("findAll: documentList is empty");
             return documentDtoList;
         }
-        for (Document document: documentList) {
+        for (Document document : documentList) {
             DocumentDto documentDto = customDocumentMapper.documentToDocumentDto(document);
             documentDtoList.add(documentDto);
         }
@@ -79,6 +78,11 @@ public class DocumentServiceImpl implements DocumentService {
 
     @Override
     public DocumentDto get(Long id) {
-        return null;
+        Document document = documentRepository.findById(id.intValue()).orElse(null);
+        if (document == null) {
+            return new DocumentDto();
+        }
+        DocumentDto documentDto = customDocumentMapper.documentToDocumentDto(document);
+        return documentDto;
     }
 }
